a673d1b70b293cccbb763df782d667cd37628a32
ChrisClark13 <krizasan@gmail.com>
2017-02-28 12:55:00 -0700

    Shooting Prototype Bullets!

---
+++ CyberDinoGame/Assets/Scripts/Architecture/ChristianC/PrototypeBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeBullet : MonoBehaviour {
+
+    public Transform owner;
+    public float speed;
+
+    // Use this for initialization
+    void Start() {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Destroy(this.gameObject);
+    }
+}

b427a152289742b82192198c616852329a208996
ChrisClark13 <krizasan@gmail.com>
2017-03-27 15:35:54 -0600

    Character Select

---
+++ Dinotron/Assets/Scripts/Architecture/ChristianC/Damage/DamageDealer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Use this component for anything in the game that deals damage.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class DamageDealer : MonoBehaviour {
+
+    public List<Transform> excludedTransforms = new List<Transform>();
+    public float damage;
+
+    protected Transform tr;
+
+    protected void Start() {
+        tr = transform;
+    }
+
+    protected List<IHitbox> hitboxes;
+
+    protected bool CanHit(Transform other) {
+        bool canhit = true;
+
+        foreach (Transform excludedTr in excludedTransforms) {
+            canhit = !(tr == excludedTr || excludedTr.IsChildOf(tr));
+        }
+
+        return canhit;
+    }
+
+    protected void FixedUpdate() {
+
+    }
+
+    protected void OnTriggerEnter(Collider collider) {
+        
+    }
+
+    protected void OnTriggerStay(Collider collider) {
+        if (hitboxes == null) {
+            hitboxes = new List<IHitbox>();
+        }
+
+        IHitbox hitbox = collider.GetComponent(typeof(IHitbox)) as IHitbox;
+        if (hitbox != null && hitbox.CanBeHit()) {
+            hitboxes.Add(hitbox);
+        }
+    }
+
+    protected void OnCollisionEnter(Collision collision) {
+        if (hitboxes == null) {
+            hitboxes = new List<IHitbox>();
+        }
+
+        IHitbox hitbox = collision.collider.GetComponent(typeof(IHitbox)) as IHitbox;
+        if (hitbox != null && hitbox.CanBeHit()) {
+            hitboxes.Add(hitbox);
+        }
+    }
+
+    protected IEnumerator ResolveDamage() {
+        yield return new WaitForFixedUpdate();
+    }
+}
---
+++ Dinotron/Assets/Scripts/Architecture/ChristianC/Damage/IHitbox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public interface IHitbox {
+
+    /// <summary>
+    /// Can this hitbox be hit right now?
+    /// </summary>
+    /// <returns></returns>
+    bool CanBeHit();
+    /// <summary>
+    /// Deal damage to this hitbox.
+    /// </summary>
+    /// <param name="damage"></param>
+    void TakeDamage(float damage);
+    /// <summary>
+    /// What hitboxes should take damage first?
+    /// For example: when a bullet hits multiple hitboxes at once, the hitbox with the lowest priority take damage and the rest are uneffected.
+    /// </summary>
+    int GetHitPriority();   
+}

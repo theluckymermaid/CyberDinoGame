84e7df75f1686702c8fbb5a8f58609bdfb109273
Justin Bezzant <justinbezzant@gmail.com>
2017-04-05 18:26:09 -0600

    Added Controller functionality from christians controllerManager

--- Dinotron/Assets/Scripts/Architecture/JustinB/OpenCloseTab.cs
+++ Dinotron/Assets/Scripts/Architecture/JustinB/OpenCloseTab.cs
@@ -6,18 +6,22 @@ using System.Collections;
 public class OpenCloseTab : MonoBehaviour {
 	[SerializeField]	//this is temporary and allows the private variable to be changed in the inspector view without breaking coding convention
 						//it will be removed in the final build once each script is set correctly.
-	private KeyCode acceptedKey; 				//this sets an KeyCode symbol that prevents the users menu from being affected when other
-												//players open their menus
+	private PlayerInputConfig inputConfig;
 	[SerializeField]
 	private bool startingActive = false;
+
+	void OnDestroy() {
+		InputManager.RemoveButtonDelegate (inputConfig.fireInput, ToggleTab);
+	}
+
 	void Start () {
-		TabToggleDel.OnPress += ToggleTab; //subscribes the method "ToggleTab" to the event OnPress from the "TabToggleDel" script
+		InputManager.AddButtonDelegate (inputConfig.fireInput, ToggleTab);
 		gameObject.SetActive (startingActive); //sets whether or not the object this script is attached to will start out disabled or enabled.
 	}
 
-	void ToggleTab(KeyCode aquiredKey) //toggles disable and enable when recieving a event call from "TabToggleDel's" "OnPress" event.
+	void ToggleTab(ButtonState button) //toggles disable and enable when recieving a event call from "TabToggleDel's" "OnPress" event.
 	{
-		if ( aquiredKey == acceptedKey) { //if the key that was pressed matches the KeyCode symbol then toggle the "pulled out" TAB view.
+		if (button.Pressed) { //if the key that was pressed matches the KeyCode symbol then toggle the "pulled out" TAB view.
 			gameObject.SetActive (!gameObject.activeSelf);
 		}	
 	}
